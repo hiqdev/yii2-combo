f8f2003d9e8b1b568bc91f8e32ceda2f5a909851
Andrii Vasyliev <sol@hiqdev.com>
2015-04-22 10:47:04 +0000

    inited

---
+++ Combo2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link    http://hiqdev.com/yii2-combo2
+ * @license http://hiqdev.com/yii2-combo2/license
+ * @copyright Copyright (c) 2015 HiQDev
+ */
+
+namespace hiqdev\combo2;
+
+/**
+ * Your class description
+ */
+class Combo2 extends \yii\base\Widget
+{
+    /**
+     * @inheritdoc
+     */
+    public function run ()
+    {
+        return "Hello!";
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Combo2 Widget for Yii 2
+-----------------------
+
+Select2 with mutual relations
+
+For more info see wiki:
+https://github.com/hiqdev/yii2-combo2/wiki

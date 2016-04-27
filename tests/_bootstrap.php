12d836797c3dd60c905cec2e3871ed85717e40a8
Andrii Vasyliev <sol@hiqdev.com>
2016-04-27 09:38:55 +0000

    inited tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Combo widget for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-combo
+ * @package   yii2-combo
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
